cbd74ee1d992da9abb1af46370153deb7983b30f
Paddyislw <pradyum0701@gmail.com>
2025-02-10 16:52:11 +0530

    Added tab title

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import Sidebar from "@/components/Sidebar";
 import WalletConnect from "./WalletConnect";
 
@@ -9,12 +10,15 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen bg-[#1a1424] text-white flex">
+      <Head>
+        <title>Dione protocol</title>
+      </Head>
       <Sidebar />
       <main className="flex-1">
         <div className="flex justify-end bg-[#29153d] h-[100px] items-center px-4">
           <WalletConnect />
         </div>
-        <div className="">{children}</div>
+        <div>{children}</div>
       </main>
     </div>
   );
